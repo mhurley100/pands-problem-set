a51bacbf8dc41408911903696d2ab20a95a28b2c
Maura Hurley <G00376337@gmit.ie>
2019-02-17 19:35:06 +0000

    Created solution 1 empty file

--- solution-1.py
+++ solution-1.py
@@ -1,1 +1,12 @@
 # Solution to problem 1
+# Sum from and to a number
+
+i = int (input("Enter a positive integer: "))
+
+Total = 0
+
+while i > 0:
+    Total = Total + i
+    i = i -1
+
+print (Total)
